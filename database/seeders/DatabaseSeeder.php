ad0786df4a497654fa94829ef6b1704b41f84c8c
tarmidzibariq <tarmidzibariq30@gmail.com>
2021-09-15 14:14:42 +0700

    Tampilan Sign UP & Sign IN

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,6 +14,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('12345678'),
+            'role'=> 'admin',
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'kasir',
+            'email' => 'kasir@gmail.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'kasir',
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'owner',
+            'email' => 'owner@gmail.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'owner',
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'member',
+            'email' => 'member@gmail.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'member',
+        ]);
     }
 }
